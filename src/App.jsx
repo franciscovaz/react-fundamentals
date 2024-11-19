6c8f84c2f2ab541575b72eb49a19231b4079995c
franciscovaz <ftomasvaz@gmail.com>
2024-11-19 22:27:05 +0000

    feat(Post): create new post component
    - add properties to a component;

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { Post } from "./Post";
+
 export function App() {
   return (
-    <h1>Hello world!</h1>
+    <>
+      <Post
+        author="Francisco Vaz"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos perferendis, modi cumque dolorum dolor earum rerum, veritatis labore unde facilis, perspiciatis ad quos iure incidunt nesciunt temporibus fugit. Assumenda, sunt?"
+      />
+
+      <Post
+        author="José Tomás"
+        content="New post"
+      />
+    </>
   )
 }
+
 
