277086ccd5eb2f65fcae7afa674af64923eac07f
franciscovaz <ftomasvaz@gmail.com>
2024-11-19 22:46:03 +0000

    feat(header) starting header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
 
 export function App() {
   return (
     <>
+      <Header />
       <Post
         author="Francisco Vaz"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos perferendis, modi cumque dolorum dolor earum rerum, veritatis labore unde facilis, perspiciatis ad quos iure incidunt nesciunt temporibus fugit. Assumenda, sunt?"
