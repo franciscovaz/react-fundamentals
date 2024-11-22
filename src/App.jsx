5d63ceb40ba18916bc96129256684001e4a7bd91
franciscovaz <ftomasvaz@gmail.com>
2024-11-22 23:55:03 +0000

    add app module styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 
+import styles from './App.module.css'
 import './global.css';
 
 export function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Francisco Vaz"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos perferendis, modi cumque dolorum dolor earum rerum, veritatis labore unde facilis, perspiciatis ad quos iure incidunt nesciunt temporibus fugit. Assumenda, sunt?"
-      />
+      
+      <div className={styles.wrapper}>
+        <aside></aside>
+        <main>
+          <Post
+          author="Francisco Vaz"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos perferendis, modi cumque dolorum dolor earum rerum, veritatis labore unde facilis, perspiciatis ad quos iure incidunt nesciunt temporibus fugit. Assumenda, sunt?"
+          />
 
-      <Post
-        author="José Tomás"
-        content="New post"
-      />
+          <Post
+            author="José Tomás"
+            content="New post"
+          />
+        </main>
+      </div>
     </>
   )
 }
