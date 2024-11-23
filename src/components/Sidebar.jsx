5d9c08e93934532eb78a5001c1d4657604b7e7a3
franciscovaz <ftomasvaz@gmail.com>
2024-11-23 00:23:52 +0000

    sidebar first styles
    edit button done

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,9 @@ import styles from './Sidebar.module.css';
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
-            <img src="https://plus.unsplash.com/premium_photo-1678566154673-a728037f3f00?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+            <img 
+                className={styles.cover}
+                src="https://plus.unsplash.com/premium_photo-1678566154673-a728037f3f00?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
             <div className={styles.profile}>
                 <strong>Francisco Vaz</strong>
