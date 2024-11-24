79f5bafae624a2887c5d495d693d7933aee2bf00
franciscovaz <ftomasvaz@gmail.com>
2024-11-24 10:54:03 +0000

    feat sidebar
    - add phosphor icons lib to add icons;
    - style avatar and profile;

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import styles from './Sidebar.module.css';
 
+import { PencilLine } from 'phosphor-react';
+
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
@@ -8,12 +10,15 @@ export function Sidebar() {
                 src="https://plus.unsplash.com/premium_photo-1678566154673-a728037f3f00?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/franciscovaz.png"/>
+                
                 <strong>Francisco Vaz</strong>
                 <span>Software Engineer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine /> 
                     Editar perfil
                 </a>
             </footer>
