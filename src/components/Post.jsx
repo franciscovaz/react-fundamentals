ecc88dd8b892a452c70fb19fb4e7b59869bc68c8
franciscovaz <ftomasvaz@gmail.com>
2024-11-26 22:57:17 +0000

    fix content template classe and add empty space

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -15,14 +15,18 @@ export function Post () {
                 <time title='25 de Novembro às 23:38h' dateTime="2024-11-25 23:38:00">Publicado à 1h</time>
             </header>
 
-            <div className={styles.container}>
-            <p>Fala galeraa 👋</p>
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
 
-            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
 
-            <p>👉 <a href="">jane.design/doctorcare</a></p>
+                <p><a href="">jane.design/doctorcare</a></p>
 
-            <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+                <p>
+                    <a href="">#novoprojeto</a>{' '}
+                    <a href="">#nlw</a>{' '}
+                    <a href="">#rocketseat</a>
+                    </p>
 
             </div>
         </article>  
