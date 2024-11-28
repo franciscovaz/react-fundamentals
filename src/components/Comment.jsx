5f5fe5d001fa147398de115ccad226498287c55a
franciscovaz <ftomasvaz@gmail.com>
2024-11-28 22:24:18 +0000

    feat (comment) add comment template without styles

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,5 +1,32 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
 export function Comment() {
     return (
-        <></>
+        <div className={styles.comment}>
+            <img src="https://github.com/franciscovaz.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Francisco Vaz</strong>
+                            <time title='25 de Novembro às 23:38h' dateTime="2024-11-25 23:38:00">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Eliminar comentário'>
+                            <Trash size={20}/>
+                        </button>
+                    </header>
+                    <p>Muito bom John, parabéns!!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />Gostos<span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
     )
 }
