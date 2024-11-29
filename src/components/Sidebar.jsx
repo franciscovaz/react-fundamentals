c02e9ec1c3d90bb42c6c92d4ae3633387bfbc650
franciscovaz <ftomasvaz@gmail.com>
2024-11-29 23:56:41 +0000

    replace avatar component and remove unused styles for old avatar usages

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 
 import { PencilLine } from 'phosphor-react';
@@ -10,7 +11,7 @@ export function Sidebar() {
                 src="https://plus.unsplash.com/premium_photo-1678566154673-a728037f3f00?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/franciscovaz.png"/>
+                <Avatar src="https://github.com/franciscovaz.png"/>
                 
                 <strong>Francisco Vaz</strong>
                 <span>Software Engineer</span>
