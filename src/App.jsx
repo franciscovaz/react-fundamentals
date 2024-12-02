9c9322ef526c244a64c5babf3b4403a9dc311b58
franciscovaz <ftomasvaz@gmail.com>
2024-12-02 23:03:54 +0000

    feat(post): add posts variable with multiple posts
    - iterate by posts and call Post component dynamicly

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,40 @@ import { Sidebar } from "./components/Sidebar"
 import styles from './App.module.css'
 import './global.css';
 
+const posts = [
+  {
+    id: '1',
+    author: {
+      avatarUrl: 'https://github.com/franciscovaz.png',
+      name: 'Francisco Vaz',
+      role: 'Software Engineer'
+    },
+    content: [
+      {type: 'paragrah', content: 'Fala galeraa 👋'},
+      {type: 'paragrah', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'}    ,
+      {type: 'link', content: 'jane.design/doctorcare'}
+    ]
+    ,
+    publishedAt: new Date('2024-12-02 23:00:00')
+  },
+  {
+    id: '2',
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego',
+      role: 'cto @Rocketseat'
+    },
+    content: [
+      {type: 'paragrah', content: 'Fala galeraa 👋'},
+      {type: 'paragrah', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'}    ,
+      {type: 'link', content: 'jane.design/doctorcare'}
+    ]
+    ,
+    publishedAt: new Date('2024-12-03 23:00:00')
+  }
+]
+
+
 export function App() {
   return (
     <>
@@ -13,9 +47,17 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post/>
-
-          <Post />
+          { posts.map(post => {
+              return (
+                <Post 
+                  key={post.id}
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              )
+            })
+          }
         </main>
       </div>
     </>
